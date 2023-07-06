f94e58aca56d3f6e109746ce4fb44ef0bee6b09e
Hernan Diego San Martin <her_san_martin@hotmail.com>
2023-07-06 19:21:58 -0300

    Se agregaron los botones e iconos al home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\DumpController;
 use Illuminate\Http\Request;
 use App\Models\Image;
+use Illuminate\View\View;
+
 
 class HomeController extends Controller
 {
@@ -22,9 +24,9 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index():View
     {
-        $images = Image::orderBy('id','desc')->get();
+        $images = Image::orderBy('id','desc')->simplePaginate(3);
         //DumpController::showArray($image);
         return view('home', [
             'images' => $images
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,21 +5,21 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             @include('includes.message')
-            
+
             <!-- Listado de todas las imagenes -->
             @foreach($images as $image)
             <div class="card pub_image">
                 <div class="card-header">
                     @if($image->user->image)
-                        @php ($avatar = $image->user->image)
+                    @php ($avatar = $image->user->image)
                     @else
-                        @php ($avatar = 'sin_imagen.jpg')
+                    @php ($avatar = 'sin_imagen.jpg')
                     @endif
                     <div class="container-avatar">
                         <img src="{{ url('../storage/app/users', ['filename'=>$avatar]) }}" alt="alt" class="avatar" />   
                     </div>
-                    
-                    
+
+
                     <div class="data-user">
                         {{ $image->user->surname.', '.$image->user->name.' | ' }}
                         <span class="nickname"><a href="">{{'@'.$image->user->nick}}</a></span>
@@ -30,18 +30,30 @@
                     <div class="image-container">
                         <img src="{{ url('../storage/app/images',['filename'=>$image->image_path]) }}" alt="alt"/>  
                     </div>
-                    <div class="likes">
-                        
-                    </div>
+                    
                     <div class="description">
                         <span class="nickname">{{ '@'.$image->user->nick }}</span>
                         <p>{{ $image->description }}</p>
                     </div>
+                    
+                    <div class="likes">
+                        <img src="{{ url('../resources/img/heart-black.png')}}" alt="alt"/>
+                    </div>
+                    
+                    <div class="comments">
+                        <a href="" class="btn btn-sm btn-warning btn-comments">Comentarios</a>
+                    </div>
                 </div>
             </div>
-            
+
             @endforeach
+            <!-- PAGINACION -->
+            {{ $images->links() }}
+
+
         </div>
+
+
     </div>
 </div>
 @endsection
